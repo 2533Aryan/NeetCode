2ac45ea04421c178faf6ecad3dfe8ac4df660e09
Aryan Raval <2533aryan@gmail.com>
2023-06-10 06:27:11 -0400

    test

--- Arrays & Hashing/ContainsDuplicateSolution.java
+++ Arrays & Hashing/ContainsDuplicateSolution.java
@@ -6,9 +6,14 @@ class ContainsDuplicateSolution {
 
     // Test
     public static void main(String[] args) {
+        // Test Class
+        ContainsDuplicateSolution test = new ContainsDuplicateSolution();
+
+        // Input 1
+        int[] input1 = {1,2,3};
+
         
         // Input 1
-        int[] input = {1,2,3};
-                        
+        int[] input2 = {1,2,3,1};                        
     }
 }
