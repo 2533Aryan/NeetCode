e4009ee96898524b9a5268ee5dcf208ce9feb810
Aryan Raval <2533aryan@gmail.com>
2023-06-10 06:48:07 -0400

    comment

--- Arrays & Hashing/ContainsDuplicateSolution.java
+++ Arrays & Hashing/ContainsDuplicateSolution.java
@@ -15,18 +15,20 @@ class ContainsDuplicateSolution {
     //     return false;
     // }
 
-    
-        // public boolean containsDuplicate(int[] nums) {
-    //     for (int i = 0; i < nums.length; i++) {
-    //         for (int j = i+1; j < nums.length; j++) {
-    //             if (nums[i] == nums[j]){
-    //                 return true;
-    //             }
-    //         }        
-    //     }
-    //     return false;
-    // }
 
+    // Logic 2
+    public boolean containsDuplicate(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i] == nums[j]){
+                    return true;
+                }
+            }        
+        }
+        return false;
+    }
+
+    
     // Test
     public static void main(String[] args) {
         // Test Class
