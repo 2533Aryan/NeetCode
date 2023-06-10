450b1ddfb235e57c2bab8dcd7ec4e73732c3dc68
Aryan Raval <2533aryan@gmail.com>
2023-06-10 06:30:07 -0400

    commet

--- Arrays & Hashing/ContainsDuplicateSolution.java
+++ Arrays & Hashing/ContainsDuplicateSolution.java
@@ -1,3 +1,7 @@
+/*
+ * Given an integer array nums, return true if any value appears at least twice in the array, 
+ * and return false if every element is distinct.
+ */
 class ContainsDuplicateSolution {
     public boolean containsDuplicate(int[] nums) {
         return false;
