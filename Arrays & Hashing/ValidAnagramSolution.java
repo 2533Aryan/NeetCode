b5b73e30b20856162f1bdf9b41e06be62d46431c
Aryan Raval <2533aryan@gmail.com>
2023-06-13 09:11:28 -0400

    test

--- Arrays & Hashing/ValidAnagramSolution.java
+++ Arrays & Hashing/ValidAnagramSolution.java
@@ -1,5 +1,12 @@
 public class ValidAnagramSolution {
     public boolean isAnagram(String s, String t) {
-        
+        return false;
     }    
+
+    // Testing
+    public static void main(String[] args) {
+        ValidAnagramSolution solution = new ValidAnagramSolution();
+        
+        solution.isAnagram(null, null);
+    }
 }
