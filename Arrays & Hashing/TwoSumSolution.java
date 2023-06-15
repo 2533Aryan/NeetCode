4e7a2fdd0937d93f48f0dce59d63232713008498
Aryan Raval <2533aryan@gmail.com>
2023-06-15 10:13:00 -0400

    logic fix

--- Arrays & Hashing/TwoSumSolution.java
+++ Arrays & Hashing/TwoSumSolution.java
@@ -20,10 +20,9 @@ public class TwoSumSolution {
             }
         }
 
-        for (int i = 0; i < nums.length; i++) {
-
+        for (int i = 0; i < nums.length-1; i++) {
             for (int j = i+1; j < nums.length; j++) {
-                
+                    
             }
         }
         
