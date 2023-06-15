271840b8eb37412c0c5be6bb5bc01a6fc96e68d9
Aryan Raval <2533aryan@gmail.com>
2023-06-15 09:33:49 -0400

    testing

--- Arrays & Hashing/TwoSumSolution.java
+++ Arrays & Hashing/TwoSumSolution.java
@@ -1,5 +1,10 @@
 public class TwoSumSolution {
     public int[] twoSum(int[] nums, int target) {
         return null;
-    }    
+    }
+    
+    // Testing
+    public static void main(String[] args) {
+        
+    }
 }
