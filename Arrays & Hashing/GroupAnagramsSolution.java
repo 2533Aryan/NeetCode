cf84abc8f40e7a71ad4b267126bd74f903c90a5d
Aryan Raval <2533aryan@gmail.com>
2023-06-18 14:52:59 -0400

    function

--- Arrays & Hashing/GroupAnagramsSolution.java
+++ Arrays & Hashing/GroupAnagramsSolution.java
@@ -2,10 +2,15 @@
  * Given an array of strings strs, group the anagrams together. 
  * You can return the answer in any order.
  */
+
+import java.util.List;
+
 public class GroupAnagramsSolution {
     
     //Logic 1:
-
+    public List<List<String>> groupAnagrams(String[] strs) {
+        return null;        
+    }
     
     // Testing
     public static void main(String[] args) {
